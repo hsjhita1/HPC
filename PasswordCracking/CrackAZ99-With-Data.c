d2f0f2d3265e20651858c3084454ce5f410f2990
Harpreet <hjhita1997@gmail.com>
2018-10-29 11:32:27 +0000

    Update PasswordCracking/CrackAZ99-With-Data.c
    Code changed to show the time at the end of the program

--- PasswordCracking/CrackAZ99-With-Data.c
+++ PasswordCracking/CrackAZ99-With-Data.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <crypt.h>
+#include <math.h>
+#include <time.h>
 
 /******************************************************************************
   Demonstrates how to crack an encrypted password using a simple
@@ -71,14 +73,35 @@ void crack(char *salt_and_encrypted){
   printf("%d solutions explored\n", count);
 }
 
+int time_difference(struct timespec *start, struct timespec *finish, long long int *difference) {
+	long long int ds = finish->tv_sec - start->tv_sec;
+	long long int dn = finish->tv_nsec - start->tv_nsec;
+
+	if (dn < 0) {
+		ds--;
+		dn += 1000000000;
+	}
+
+	*difference = ds * 1000000000 + dn;
+	return !(*difference > 0);
+}
+
 int main(int argc, char *argv[]){
-  int i;
+struct timespec start, finish;
+long long int time_elapsed;
+clock_gettime(CLOCK_MONOTONIC, &start);
 
-  for(i=0;i<n_passwords;i<i++) {
-    crack(encrypted_passwords[i]);
-  }
+int i;
 
-  return 0;
+for(i=0;i<n_passwords;i<i++) {
+	crack(encrypted_passwords[i]);
+}
+
+clock_gettime(CLOCK_MONOTONIC, &finish);
+time_difference(&start, &finish, &time_elapsed);
+printf("Time elasped was %lldns or %0.9lfs\n", time_elapsed, (time_elapsed / 1.0e9));
+
+return 0;
 }
 
 
